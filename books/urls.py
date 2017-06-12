ea19049477f2ce42df5e60ab3c1479caa15ba2a4
Chris Hidalgo <chidalgo088@gmail.com>
2017-06-12 15:32:37 -0400

    Update urls.py

--- books/urls.py
+++ books/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^$', views.list_books),
+    url(r'^$', views.list_books, name="home"),
     url(r'^(?P<id>\d)/$', views.detail, name="search"),
+    url(r'^author/(?P<id>\d)/$', views.author_books, name="book_author")
 
 
 ]
